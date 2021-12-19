04e3aed156ae5e82b683d873b43741ff6cb16f9c
krzysztofzapart <krzysztofzapart@icloud.com>
2021-12-19 21:05:10 +0100

    Initial Commit

---
+++ src/main/java/pl/kzapart/item/ItemApplication.java
@@ -0,0 +1,13 @@
+package pl.kzapart.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemApplication.class, args);
+	}
+
+}
